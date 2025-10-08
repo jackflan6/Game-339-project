cabc026c8b534a56d9f9bf5537486d2923aeeb04
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-10-07 17:26:02 -0700

    added some basic interfaces and the start to a turn manager

---
+++ CardGame339/Assets/Scripts/IEnemy.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+public class IEnemy
+{
+    public List<>
+    public virtual void damage(int val) { }
+    public virtual void onNewTurn() { }
+    public virtual void applyStatisEffect() { }
+
+    
+}
