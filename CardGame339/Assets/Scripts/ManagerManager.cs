ee321e74de0a4b6a5c9db81fe3f17e4a3c4194c7
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-10-12 18:56:08 -0700

    saved

--- CardGame339/Assets/Scripts/ManagerManager.cs
+++ CardGame339/Assets/Scripts/ManagerManager.cs
@@ -5,30 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ManagerManager : MonoBehaviour
+public static class ManagerManager
 {
-    [SerializeField]
-    public List<IManager> managers;
-
-    private void Start()
-    {
-        foreach (IManager m in managers)
-        {
-            m.start();
-        }
-    }
-    private void Awake()
-    {
-        foreach (IManager m in managers)
-        {
-            m.awake();
-        }
-    }
-    private void Update()
-    {
-        foreach (IManager m in managers)
-        {
-            m.update();
-        }
-    }
+    public static Lazy<> = ()
 }
