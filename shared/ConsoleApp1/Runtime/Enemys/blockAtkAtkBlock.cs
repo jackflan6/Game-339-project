42df37460f191b917c198aeb4fe7e76e132a37d6
HyruleHero654 <charfost98@gmail.com>
2025-10-22 15:27:17 -0700

    Created Many Different Enemy Types
    They all use some variation of our four actions: attack, make shield, taunt, or burn. Enemies have a set pattern that they follow except for theBillowedAss that still randomly generates an action on its turn.

---
+++ shared/ConsoleApp1/Runtime/Enemys/blockAtkAtkBlock.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+
+public class specificPatternEnemy : Enemy
+{
+    readonly CombatSystem combatSystem;
+    readonly IDialog dialogSys;
+    readonly EnemyManager enemyManager;
+    readonly IRandom random;
+
+    private int processionOfActions;
+    //::::Important::::
+    //It is nessasary to have every card have a static int for its ID.
+    //This need to be called "enemyID" and reflected in the selectableCard object.
+    //It is not forced by the interface so you just need to remember
+    public static int enemyID = 6;
+    
+    #if !NOT_UNITY
+    public specificPatternEnemy()
+    {
+         combatSystem = ManagerManager.Resolve<CombatSystem>();
+         dialogSys = ManagerManager.Resolve<IDialog>();
+         enemyManager = ManagerManager.Resolve<EnemyManager>();
+         random = ManagerManager.Resolve<IRandom>();
+         
+    }
+    #endif
+
+    public specificPatternEnemy(CombatSystem combatSystem, IDialog iDialog, EnemyManager enemyManager, IRandom rand)
+    {
+        this.combatSystem = combatSystem;
+        dialogSys = iDialog;
+        this.enemyManager = enemyManager;
+        random = rand;
+    }
+
+    public override int Attack { get; set; } = 5;
+    public override int Defense { get; set; } = 5;
+    public override int burnAttackDamage { get; } = 0;
+    public override ValueHolder<int> HP { get; set; } = 5;
+
+    public override void DoAction(Player player, Enemy enemy)
+    {
+        if (processionOfActions == 0)
+        {
+            combatSystem.GenerateEnemyShield(enemy);
+        }
+        else if (processionOfActions == 1)
+        {
+            combatSystem.DealDamageToPlayer(player, enemy);
+        }
+        else if (processionOfActions == 2)
+        {
+            combatSystem.DealDamageToPlayer(player, enemy);
+        }
+        else if (processionOfActions==3)
+        {
+            combatSystem.GenerateEnemyShield(enemy);
+        }
+        
+
+        processionOfActions++;
+        if(processionOfActions>3)
+        {
+            processionOfActions = 0;
+        }
+    }
+
+}
---
+++ shared/ConsoleApp1/Runtime/Enemys/shieldAttackBoss.cs
@@ -0,0 +1,48 @@
+using System;
+public class shieldAttackBoss : Enemy
+{
+    readonly CombatSystem combatSystem;
+    readonly IDialog dialogSys;
+    readonly EnemyManager enemyManager;
+    readonly IRandom random;
+
+    private int processionOfActions;
+    //::::Important::::
+    //It is nessasary to have every card have a static int for its ID.
+    //This need to be called "enemyID" and reflected in the selectableCard object.
+    //It is not forced by the interface so you just need to remember
+    public static int enemyID = 0;
+    
+    #if !NOT_UNITY
+    public shieldAttackBoss()
+    {
+         combatSystem = ManagerManager.Resolve<CombatSystem>();
+         dialogSys = ManagerManager.Resolve<IDialog>();
+         enemyManager = ManagerManager.Resolve<EnemyManager>();
+         random = ManagerManager.Resolve<IRandom>();
+         
+    }
+    #endif
+
+    public shieldAttackBoss(CombatSystem combatSystem, IDialog iDialog, EnemyManager enemyManager, IRandom rand)
+    {
+        this.combatSystem = combatSystem;
+        dialogSys = iDialog;
+        this.enemyManager = enemyManager;
+        random = rand;
+    }
+
+    public override int Attack { get; set; } = 3;
+    public override int Defense { get; set; } = 3;
+    public override int burnAttackDamage { get; } = 0;
+    public override ValueHolder<int> HP { get; set; } = 10;
+
+    public override void DoAction(Player player, Enemy enemy)
+    {
+        
+        combatSystem.GenerateEnemyShield(enemy);
+        combatSystem.DealDamageToPlayer(player, enemy);
+        
+    }
+
+}
