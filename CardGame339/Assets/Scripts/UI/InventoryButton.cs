f037e6c150a3f5d2dcdb4fe2321b38324a01b411
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-11-04 21:47:18 -0800

    added ui

---
+++ CardGame339/Assets/Scripts/UI/InventoryButton.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class InventoryButton : MonoBehaviour
+{
+    public GameObject card;
+
+
+}
--- CardGame339/Assets/Scripts/UnityExclusive/UnityInventory.cs
+++ CardGame339/Assets/Scripts/UnityExclusive/UnityInventory.cs
@@ -1,19 +1,33 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnityInventory : MonoBehaviour
 {
     Inventory inventory;
+    public List<GameObject> allCardsPrefabs;
+    public GameObject inventorybutton;
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        reloadOptions();
     }
 
     public void AddCardToInventory(Card card)
     {
         inventory.AddCardsInInventory(card);
     }
-    
-    
+
+    public List<GameObject> buttons;
+    public void reloadOptions()
+    {
+        GameObject viewport = GameObject.FindGameObjectWithTag("inventory viewport");
+        foreach (GameObject card in allCardsPrefabs)
+        {
+            GameObject button = Instantiate(inventorybutton,viewport.transform);
+            button.GetComponent<Image>().sprite = card.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
 
 }
